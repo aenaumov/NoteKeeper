ac137239f4877b9242616f76456d36062da0beb3
Alexander Naumov <a.e.naumov@yandex.ru>
2023-06-09 14:54:10 +0300

    implement UserDetailsService in UserService

--- resource/src/main/java/ru/education/myproject1/service/UserService.java
+++ resource/src/main/java/ru/education/myproject1/service/UserService.java
@@ -1,9 +1,10 @@
 package ru.education.myproject1.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.education.myproject1.model.dto.UserCreateDto;
 import ru.education.myproject1.model.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
     UserDto createUser (UserCreateDto userCreateDto);
 
