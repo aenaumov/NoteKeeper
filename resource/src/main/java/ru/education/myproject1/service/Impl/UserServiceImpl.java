c3192a2384aed5e92261a0f41c9549d66f22abdd
Alexander Naumov <a.e.naumov@yandex.ru>
2023-06-09 14:55:19 +0300

    create loadUserByUsername in UserServiceImpl

--- resource/src/main/java/ru/education/myproject1/service/Impl/UserServiceImpl.java
+++ resource/src/main/java/ru/education/myproject1/service/Impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package ru.education.myproject1.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.education.myproject1.model.User;
 import ru.education.myproject1.model.dto.UserCreateDto;
 import ru.education.myproject1.model.dto.UserDto;
@@ -11,6 +14,7 @@ import ru.education.myproject1.service.UserService;
 import ru.education.myproject1.util.UserMapper;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -26,5 +30,24 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
 
         return UserMapper.toUserDto(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+//                .disabled(!user.getVerified())
+                .disabled(false)
+                .accountExpired(false)
+                .accountLocked(false)
+                .roles(user.getUserRole())
+                .build();
     }
 }
