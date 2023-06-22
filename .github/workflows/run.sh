f588deb4ec6574add798b03eb174df20c712ded7
Alexander Naumov <a.e.naumov@yandex.ru>
2023-06-22 13:35:46 +0300

    refactor  api-tests.yml

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,2 +1,2 @@
-#nohup mvn spring-boot:run -Dspring-boot.run.profiles=ci >> console.log 2>&1 &
-nohup mvn spring-boot:run &>> console.log 2>&1 &
+nohup mvn spring-boot:run -Dspring-boot.run.profiles=ci >> console.log 2>&1 &
+#nohup mvn spring-boot:run &>> console.log 2>&1 &
