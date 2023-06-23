c968d2bcf1b5cef082a7f795c82847f36008882d
Alexander Naumov <a.e.naumov@yandex.ru>
2023-06-23 13:03:06 +0300

    refactor api-tests

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,1 +1,4 @@
 nohup mvn spring-boot:run &
+#mvn exec:java -Dexec.mainClass="ru.education.myproject1.MyProject1Application"
+
+#mvn spring-boot:run -Dspring-boot.run.mainClass=resource.ru.education.myproject1.MyProject1Application
