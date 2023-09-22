6387c1f82f8e6b6b4a3f60c97db9ef8e822a603a
Alexander Naumov <a.e.naumov@yandex.ru>
2023-09-22 13:48:32 +0300

    create AuthServiceImpl

---
+++ router/src/main/java/ru/education/myproject1/service/Impl/AuthServiceImpl.java
@@ -0,0 +1,22 @@
+package ru.education.myproject1.service.Impl;
+
+import reactor.core.publisher.Mono;
+import ru.education.myproject1.client.AuthWebClient;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.education.myproject1.service.AuthService;
+
+@Service
+@Transactional(readOnly = true)
+@AllArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final AuthWebClient authWebClient;
+
+    @Override
+    public Mono<String> getJwt(String authHeader) {
+        return authWebClient.getJwt(authHeader);
+    }
+
+}
