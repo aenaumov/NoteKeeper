c8462a644b4f2afcf7760309b571435f3a0deaf3
Alexander Naumov <a.e.naumov@yandex.ru>
2023-10-05 16:14:09 +0300

    refactor run

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,4 +1,10 @@
+cd auth
+nohup mvn spring-boot:run &
+
 cd resource
+nohup mvn spring-boot:run &
+
+cd router
 nohup mvn spring-boot:run &
 #mvn exec:java -Dexec.mainClass="ru.education.myproject1.MyProject1Application"
 
