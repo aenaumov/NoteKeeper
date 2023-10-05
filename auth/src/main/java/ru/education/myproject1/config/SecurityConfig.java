b909906328263ead372fae95231ff92703c5e09c
Alexander Naumov <a.e.naumov@yandex.ru>
2023-10-05 15:34:21 +0300

    refactor SecurityConfig

--- auth/src/main/java/ru/education/myproject1/config/SecurityConfig.java
+++ auth/src/main/java/ru/education/myproject1/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,9 +23,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers("/auth/jwt/get")
+                                .requestMatchers("/auth/token")
                                 .hasAnyRole("ADMIN", "USER")
                                 .anyRequest()
                                 .permitAll()
