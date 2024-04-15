4ff15eeeed0f57cade4963263e9aa0ceb6255feb
Alexander Naumov <a.e.naumov@yandex.ru>
2024-04-15 13:37:12 +0300

    refactor remove Transaction

--- resource/src/main/java/ru/education/myproject1/service/Impl/NoteServiceImpl.java
+++ resource/src/main/java/ru/education/myproject1/service/Impl/NoteServiceImpl.java
@@ -2,14 +2,12 @@ package ru.education.myproject1.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import ru.education.myproject1.model.Note;
 import ru.education.myproject1.repo.NoteRepository;
 import ru.education.myproject1.service.NoteService;
 
 @Service
-@Transactional
 public class NoteServiceImpl implements NoteService {
 
     @Autowired
@@ -17,7 +15,6 @@ public class NoteServiceImpl implements NoteService {
 
 
     @Override
-    @Transactional(readOnly = true)
     public Flux<Note> getNotes(Long id, Long user_id, String role) {
 
 //        TODO проверка id and role
