5e81ed7c1ca2c262d07afd63c98ee5923afbbe19
Alexander Naumov <a.e.naumov@yandex.ru>
2024-04-15 13:44:24 +0300

    refactor add versioning

--- auth_client/src/main/java/ru/education/myproject1/model/User.java
+++ auth_client/src/main/java/ru/education/myproject1/model/User.java
@@ -2,6 +2,7 @@ package ru.education.myproject1.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.annotation.Id;
@@ -23,4 +24,8 @@ public class User {
     private String password;
     @Column("USER_ROLE")
     private String userRole;
+
+    @Column("VERSION")
+    @Version
+    private Long version;
 }
